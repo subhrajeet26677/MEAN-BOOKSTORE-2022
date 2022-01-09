0a4c617d83b25b5195f01567a98fdbb2267f922c
subhrajeet26677 <72700604+subhrajeet26677@users.noreply.github.com>
2022-01-09 12:18:16 +0530

    1st session completed

--- book-store/src/app/app.module.ts
+++ book-store/src/app/app.module.ts
@@ -1,18 +1,23 @@
+// import { NgModule } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  //imports module it depends on
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AuthModule
   ],
   providers: [],
+  //entry point for module
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+//i have to delete the component and make a new one
